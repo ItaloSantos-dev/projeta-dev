01aeb0a5396b29fc6450a2b27ab59223e71ec36e
Italo Santos <italosantoscont@gmail.com>
2026-02-25 11:59:23 -0300

    feat: implements the interface of UserDetails and UserDetailsService and your functions

---
+++ src/main/java/santzin/projeta/dev/service/UserService.java
@@ -0,0 +1,20 @@
+package santzin.projeta.dev.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import santzin.projeta.dev.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByEmail(username);
+    }
+}
