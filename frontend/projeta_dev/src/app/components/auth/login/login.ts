d14de2afd512c1e0fd6ff78fc13be104fbaac998
Italo Santos <italosantoscont@gmail.com>
2026-03-02 17:24:20 -0300

    refactor: fix name of properties of login dto

--- frontend/projeta_dev/src/app/components/auth/login/login.ts
+++ frontend/projeta_dev/src/app/components/auth/login/login.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 import { RouterLink } from "@angular/router";
+import { AuthService } from '../../../service/auth-service';
+import { LoginDTO } from '../../../../types/DTO/login-dto';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +11,30 @@ import { RouterLink } from "@angular/router";
   styleUrl: './login.css',
 })
 export class Login {
+
+  authService = inject(AuthService)
+
   formLogin = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
 
-  
+  private createLoginDTO(){
+    const data:LoginDTO = {
+      email: this.formLogin.get('email')?.value,
+      
+    }
+  }
+
+  ngSubmit(){
+    this.authService.backApi.login().subscribe({
+      next:(dado) => {
+        console.log("Passou. TOKEN: " + dado);
+      },
+      error:(erro)=>{
+        console.log("Erro");
+        
+      }
+    })
+  }
 }
--- frontend/projeta_dev/src/types/DTO/login-dto.ts
+++ frontend/projeta_dev/src/types/DTO/login-dto.ts
@@ -1,4 +1,4 @@
 export interface LoginDTO{
-    name:string,
+    email:string,
     password:string
 }
