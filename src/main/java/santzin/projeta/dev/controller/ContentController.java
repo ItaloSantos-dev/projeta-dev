fda775c8d15d4b46893e84ba89d3b9077a8afae2
Italo Santos <italosantoscont@gmail.com>
2026-03-03 09:54:48 -0300

    feat: create a content Controller

---
+++ src/main/java/santzin/projeta/dev/controller/ContentController.java
@@ -0,0 +1,29 @@
+package santzin.projeta.dev.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import santzin.projeta.dev.DTOs.content.ContentRequestDTO;
+import santzin.projeta.dev.DTOs.content.ContentResponseDTO;
+import santzin.projeta.dev.DTOs.content.ContentResponseSimplifiedDTO;
+import santzin.projeta.dev.service.ContentService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("contents")
+public class ContentController {
+    @Autowired
+    private ContentService contentService;
+
+    @GetMapping("/most-popular")
+    private ResponseEntity<List<ContentResponseSimplifiedDTO>> getMostPopularContents(){
+        return ResponseEntity.ok(this.contentService.getMostPopularContents());
+    }
+
+    @PostMapping()
+    private ResponseEntity<ContentResponseSimplifiedDTO> getContent(@RequestBody ContentRequestDTO contentRequestDTO){
+        return ResponseEntity.ok(this.contentService.getContent(contentRequestDTO));
+    }
+}
