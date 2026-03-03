b695eddfa23ef6145982a6c349e35d8a200e0994
Italo Santos <italosantoscont@gmail.com>
2026-03-03 08:12:24 -0300

    refactor: add a field of esperience of user in register page

--- frontend/projeta_dev/src/app/components/auth/register/register.ts
+++ frontend/projeta_dev/src/app/components/auth/register/register.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { AuthService } from '../../../service/auth-service';
+import { UserExperienceLevel } from '../../../../types/enums/user-experience-level';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+
+  authService = inject(AuthService)
 
   formRegister = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     telephoneNumber: new FormControl('', Validators.required),
+    experienceLevel: new FormControl('', [Validators.required]),
     principalStack: new FormControl('', Validators.required)
   })
+
+  
 
 }
