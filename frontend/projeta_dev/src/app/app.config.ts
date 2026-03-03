dd78096e26a2da8a3bb16c1e5ea1f14dde3642b0
Italo Santos <italosantoscont@gmail.com>
2026-03-03 16:06:23 -0300

    chore: install a markdown ad tailwindcss typograph dependencies

--- frontend/projeta_dev/src/app/app.config.ts
+++ frontend/projeta_dev/src/app/app.config.ts
@@ -1,15 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TokenInterceptor } from '../security/interceptors/token-interceptor';
 
+import { MarkdownModule } from 'ngx-markdown';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([TokenInterceptor])),
+    importProvidersFrom(
+      MarkdownModule.forRoot()
+    )
 
   ]
 };
