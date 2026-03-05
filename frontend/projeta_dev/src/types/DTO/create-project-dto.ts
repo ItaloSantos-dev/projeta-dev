bbffaef90191b9c3693760536c2c0d3cabb1fa38
Italo Santos <italosantoscont@gmail.com>
2026-03-04 21:25:46 -0300

    feat: create dto in front end for create project

---
+++ frontend/projeta_dev/src/types/DTO/create-project-dto.ts
@@ -0,0 +1,9 @@
+export interface CreateProjectDTO {
+  title: string;
+  imgUrl: string;
+  description: string;
+  stack: string;
+  inputType: string;
+  repositoryLink: string;
+  principalPosition: string;
+}
