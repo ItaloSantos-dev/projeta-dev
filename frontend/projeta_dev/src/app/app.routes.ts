d0e246de60d6d66341f1dff608a5e0308f576638
Italo Santos <italosantoscont@gmail.com>
2026-03-04 21:11:16 -0300

    feat: create a page for create new project

--- frontend/projeta_dev/src/app/app.routes.ts
+++ frontend/projeta_dev/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Login } from './components/auth/login/login';
 import { Register } from './components/auth/register/register';
 import { Dashboard } from './components/dashboard/dashboard';
 import { ShowContent } from './components/contents/show-content/show-content';
+import { CreateProject } from './components/projects/create-project/create-project';
 
 export const routes: Routes = [
     {path:"", component:Home},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     {path:"register", component:Register},
     {path:"dashboard/:page", component:Dashboard},
     {path:"contents/show/:owner/:slug", component:ShowContent},
+    {path:"projects/create", component:CreateProject}
 ];
