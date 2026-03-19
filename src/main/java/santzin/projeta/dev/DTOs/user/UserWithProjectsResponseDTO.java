40d693a263d7b524debae780aba22b3e82f2507c
Italo Santos <italosantoscont@gmail.com>
2026-03-19 18:03:10 -0300

    feat: create a userWithProject response dto

---
+++ src/main/java/santzin/projeta/dev/DTOs/user/UserWithProjectsResponseDTO.java
@@ -0,0 +1,29 @@
+package santzin.projeta.dev.DTOs.user;
+
+import santzin.projeta.dev.DTOs.hability.HabilityResponseDTO;
+import santzin.projeta.dev.DTOs.project.ProjectResponseDTO;
+import santzin.projeta.dev.model.enums.UserExperienceLevel;
+
+import java.util.List;
+
+public record UserWithProjectsResponseDTO(
+        Long id,
+        String name,
+        String username,
+        UserExperienceLevel experienceLevel,
+        String principalStack,
+        List<ProjectResponseDTO> myProjects,
+        String about,
+        String coverUrl,
+        String perfilUrl,
+        String link1,
+        String link2,
+        String link3,
+        String link4,
+        String link5,
+        List<HabilityResponseDTO> habilitys,
+        Integer notificationsCount,
+        List<String> following,
+        List<String> followers
+) {
+}
